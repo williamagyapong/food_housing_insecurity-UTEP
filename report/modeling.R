364a1afc1d734b5fe98dfc049aaaa50a4dfbb12a
William Ofosu Agyapong <williamofosuagyapong@gmail.com>
2021-12-07 04:04:16 -0700

    Trained models

--- report/modeling.R
+++ report/modeling.R
@@ -10,15 +10,31 @@ source("modeling_functions.R")
 
 
 #------------- Train Models ------------------------
+
+# Modeling Housing Insecurity Responses
 pa_models <- modeling("permanent_address")
 
 sne_models <- modeling("spent_night_elsewhere", 
                 c("earth","glm","knn","svmLinear","svmRadial"))
 
+# Modeling Food Insecurity Responses
 FI_q26_models <- modeling("FI_q26")
+
+FI_q27_models <- modeling("FI_q27")
+
+FI_q28_models <- modeling("FI_q28")
+
+FI_q30_models <- modeling("FI_q30")
+
+FI_q31_models <- modeling("FI_q31")
 
 
 #------------- Save Results ------------------------
-save(pa_models, "trained_models.RData")
+save(sne_models, pa_models, FI_q26_models, FI_q27_models,
+     FI_q28_models, FI_q30_models, FI_q31_models,
+     file = "trained_models.RData"
+     )
+
+# load("trained_models.RData")
 
 
